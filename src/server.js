5401a776ba0745138134e8316ea5c82df6d90979
Geovani Auler <geovaniauler@gmail.com>
2024-10-15 09:50:32 -0300

    Altera o Server

--- src/server.js
+++ src/server.js
@@ -18,10 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas a API do Allbooks")
-  console.log("API disponível através da url http://localhost:8000")
-=======
-  console.log("Acesse essa API em http://localhost:8000")
->>>>>>> 279e3db297af0f134631d2f20f7e6d90df93579d
+  console.log("API disponível em http://localhost:8000")
 })
